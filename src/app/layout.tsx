166230f7f0e62f5ccc03ec3c1f0f293b14ea643f
Aking <www.aking016@gmail.com>
2025-09-20 15:24:56 +0330

    👌 MINOR IMPROVE: removed unneeded import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import ThemeProvider from "@/context/theme-provider";
 import { CssBaseline } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import type { Metadata } from "next";
-import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Create Next App",
